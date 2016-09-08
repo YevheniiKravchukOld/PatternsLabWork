294e5d5d3189ba7b979a47d1984d88853c1187c7
Evgenii Kravchuk <zhenia.kravchuk@gmail.com>
2016-09-08 19:55:35 +0300

    Implement Prorotype class

--- src/main/java/prototype/Prototype.java
+++ src/main/java/prototype/Prototype.java
@@ -4,4 +4,9 @@ package main.java.prototype;
  * Created by evgenii on 07.09.16.
  */
 public class Prototype {
+
+    public AbstractDish getClone(AbstractDish dishSample) {
+        return dishSample.makeCopy();
+    }
+
 }
